03ced0651ed5232ba70c7b6d3e578a822ce27870
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-04-16 15:26:02 +0200

    Initial commit w/ basic draft of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miaa::Application.config.secret_token = '329e1fed0678a827cd1da0962267b0e9bce9648db6c217286561475db3be663e7e7e7b1b56946e99b70f6d742679b400785bfb9731e234a776af645f4bd4c6b7'
