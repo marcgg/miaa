f60fa64681814c45593f071b19e71aacdac26b19
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-04-16 17:20:53 +0200

    Intense CSS action; Action links with dummy pages while waiting for copy

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -2,4 +2,27 @@ class WebsiteController < ApplicationController
   def index
   end
 
+  def presentation
+  end
+  
+  def join
+  end
+  
+  def news
+  end
+  
+  def give
+  end
+  
+  def contact
+  end
+  
+  def press
+  end
+  
+  def photos
+  end
+  
+  def a_day_there
+  end
 end
